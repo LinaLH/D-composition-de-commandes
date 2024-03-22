cf874f2b77ecffa35e0ee55e92f000729af0705a
LinaLH <121266302+LinaLH@users.noreply.github.com>
2024-03-22 15:07:47 +0100

    Corrections

--- projet.jl
+++ projet.jl
@@ -1,5 +1,6 @@
 using JuMP
-using CBC
+using Cbc
+#Pkg.add("Cbc")
 
 # Assurez-vous que le fichier lecture.jl est dans le même répertoire
 include("lecture.jl")
@@ -11,56 +12,55 @@ include("lecture.jl")
 #donnees = lecture(lines)
 
 # Initialiser le modèle JuMP avec le solveur CBC
-model = Model(CBC.Optimizer)
+model = Model(Cbc.Optimizer)
 
 # Supposons que les données soient maintenant dans la variable `donnees`
 # Par exemple: donnees.N, donnees.R, donnees.O, etc.
 
 # Variables de décision
-@variable(model, x[1:donnees.R, 1:donnees.P], Bin)  # Affectation des racks aux préparateurs
-@variable(model, y[1:donnees.O, 1:donnees.P], Bin)  # Affectation des commandes aux préparateurs
-@variable(model, 0 <= v[1:donnees.O] <= 1)
-@variable(model, 0 <= u[1:donnees.R] <= 1)
+@variable(model, x[1:Data.R, 1:Data.P], Bin)  # Affectation des racks aux préparateurs
+@variable(model, y[1:Data.O, 1:Data.P], Bin)  # Affectation des commandes aux préparateurs
+@variable(model, 0 <= v[1:Data.O] <= 1)
+@variable(model, 0 <= u[1:Data.R] <= 1)
 
 
 # Contraintes
 
 #1
-for o in donnees.FO
-    @constraint(model, sum(x[o, p] for p in 1:donnees.P) == 1)
+for o in Data.FO
+    @constraint(model, sum(x[o, p] for p in 1:Data.P) == 1)
 end
 
 #2
-for o in donnees.SO
-    @constraint(model, sum(x[o, p] for p in 1:donnees.P) == v0)
+for o in Data.SO
+    @constraint(model, sum(x[o, p] for p in 1:Data.P) == v0)
 end
 
 #3
-for r in donnees.RS
-    @constraint(model, sum(y[r, p] for p in 1:donnees.P) == ur)
+for r in Data.RS
+    @constraint(model, sum(y[r, p] for p in 1:Data.P) == ur)
 end
 
 #4
-for p in donnees.SO
-    @constraint(model, sum(x[o, p] for o in 1:donnees.O) <= donnees.Capa[p])
+for p in Data.SO
+    @constraint(model, sum(x[o, p] for o in 1:Data.O) <= Data.Capa[p])
 end
 
 #5
-for r in donnees.RS for o in 1:donnees.O
-    @constraint(model, sum(s[i, r]*y[r, p] for i in 1:donnees.N for p in 1:donnees.P) <= sum(q[i, o]*x[o, p] for i in 1:donnees.N for p in 1:donnees.P))
+for r in Data.RS for o in 1:Data.O
+    @constraint(model, sum(s[i, r]*y[r, p] for i in 1:Data.N for p in 1:Data.P) <= sum(q[i, o]*x[o, p] for i in 1:Data.N for p in 1:Data.P))
 end
 
 # Fonction objectif
-@objective(model, Min, sum(abs(S)+1*ur for i in 1:donnees.R) - sum(v[o] for o in donnees.SO))
+@objective(model, Min, sum(abs(S)+1*ur for i in 1:Data.R) - sum(v[o] for o in Data.SO))
 
 # Résoudre le modèle
 optimize!(model)
 
 # Afficher les résultats
-# Vous pouvez afficher les résultats de l'optimisation ici, par exemple:
 println("Solution optimale:")
-for p in 1:donnees.P
-    for r in 1:donnees.R
+for p in 1:Data.P
+    for r in 1:Data.R
         if value(x[r,p]) > 0.5
             println("Rack $r assigné au préparateur $p")
         end
